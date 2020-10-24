f59675244e88cc01b000f6b43698d7a06b317aaf
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-10-24 05:04:19 -0600

    Add handling for usernmae

--- web/smPart1/registration/registrationController.js
+++ web/smPart1/registration/registrationController.js
@@ -13,7 +13,7 @@ export default class RegistrationController {
 		this.parentElement = document.getElementById(parentId);
 		this.registrationView = new RegistrationView(this.parentElement);
 		this.registrationModel = new RegistrationModel();
-		this.usernameElement = '';
+		this.usernameElement, this.username = '';
 	}
 
 	/****************************************
@@ -38,6 +38,99 @@ export default class RegistrationController {
 	* This handles user interaction and form validation
 	****************************************/
 	addRegistrationListeners() {
+		this.usernameElement.addEventListener('keydown', ()=> {
+			this.loginView.getRidOfErrorMessage();
+		});
 
+		this.usernameElement.addEventListener('keyup', (event)=> {
+			this.username = event.target.value;
+			const isValidUsername = this.isValidUsername();
+			if (!isValidUsername && !this.loginView.isErrorMessageDisplayed()) {
+				this.loginView.renderErrorMesssage('un');
+			} else if (isValidUsername && this.loginView.isErrorMessageDisplayed()) {
+				this.loginView.getRidOfErrorMessage();
+			}
+		});
+
+		// Email Element
+
+		// First name element
+
+		// Last Name element
+
+		// Password Element
+
+		// handle submit events
+
+	}
+
+	/**************************************
+	* This code checks the following:
+	*     - the username doesn't exceed 100 chars
+	*     - first character is an alpha character
+	*     - security
+	***************************************/
+	isValidUsername() {
+		if (this.username.length > 100) {
+			return false;
+		}
+
+		this.username = this.escapeHtml();
+
+		if (!this.isAlpha() && !this.isEmpty()) {
+			return false;
+		}
+
+		if (this.isWhiteSpace()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**************************************
+	* Credit for the JavaScript version of htmlspecialchars goes to 
+	* 'Kip' on stackoverflow: https://stackoverflow.com/questions/1787322/htmlspecialchars-equivalent-in-javascript
+	***************************************/
+	escapeHtml() {
+	    var map = {
+	        '&': '&amp;',
+	    	'<': '&lt;',
+	    	'>': '&gt;',
+	    	'"': '&quot;',
+	    	"'": '&#039;'
+	  	};
+  
+  		return this.username.replace(/[&<>"']/g, function(m) { return map[m]; });
+	}
+
+	/*************************************
+	* This method checks if the first letter is an alpha character 
+	**************************************/
+	isAlpha() {
+		const char = this.username.charCodeAt(0);
+	    if (!(char > 64 && char < 91) && // upper alpha (A-Z)
+	        !(char > 96 && char < 123)) { // lower alpha (a-z)
+	      return false;
+	    }
+	  	return true;
+	}
+
+	/*************************************
+	* This method checks if the first letter is empty
+	**************************************/
+	isEmpty() {
+		const char = this.username.charCodeAt(0);
+		if (!(Number.isNaN(char))) {
+			return false;
+		}
+		return true;
+	}
+
+	/************************************
+	* This method checks for whitespace in the string
+	*************************************/
+	isWhiteSpace() {
+		return this.username.indexOf(' ') >= 0;
 	}
 }
--- web/smPart1/registration/registrationView.js
+++ web/smPart1/registration/registrationView.js
@@ -23,12 +23,68 @@ export default class RegistrationView {
 	* This method renders a single div 
 	* input field
 	**********************************/
-	renderInputDiv(label) {
+	renderInputDiv(child) {
 		const div = document.createElement('div');
 		div.innerHTML = (`
-			<label for='${label.toLowerCase()}'>${label}</label><br/>
-			<input type='${label === 'Password' ? 'password' : 'text'}' class='light' name='${label.toLowerCase()}' autocomplete='off'>
+			<label for='${child.toLowerCase()}'>${child}</label><br/>
+			<input type="${child === 'Password' ? 'password' : 'text'}" class='light' name='${child.toLowerCase()}' autocomplete='off'>
 		`);
+		console.log(div);
 		return div;
+	}
+
+	/********************************
+	* This method gets rid of the error message(s) 
+	* that exist below the important form-children nodes
+	*********************************/
+	getRidOfErrorMessage() {
+		if (this.formParentElement.childNodes.length > 5) { 
+			for (let i = this.formParentElement.childNodes.length - 1, min = 4; i > min; i--) {
+				this.formParentElement.removeChild(this.formParentElement.children[i]);
+			}
+		}
+	}
+
+	/*******************************
+	* This method checks if there are 
+	* more child nodes than there should be. If 
+	* there are more, then error messsages exist
+	********************************/
+	isErrorMessageDisplayed() {
+		if (this.formParentElement.childNodes.length > 5) {
+			return true;
+		}
+		return false;
+	}
+
+	/******************************
+	* This method renders the error message 
+	* as specified by the client to this
+	* method.
+	*******************************/
+	renderErrorMessage(type) {
+		let newChild = ()=> {
+			const span = document.createElement('span');
+			span.setAttribute('id', 'error');
+			span.innerHTML = `<br/>Error:`;
+			switch (type) {
+				case 'un':
+					span.innerHTML += ` Username must (1) start with a letter from the alphabet, (2) limit itself to 100 characters, and (3) contain no white spaces.`;
+					break;
+				case 'inUn':
+					span.innerHTML += ` Username already exists in the database. Please specifiy another username.`;
+					break;
+				case 'un-e':
+					span.innerHTML += ` Please enter a username`;
+					break;
+				case 'pw':
+					span.innerHTML += ` Please enter a password`;
+					break;
+				default:
+					span.innerHTML += '';
+			}
+			return span;
+		}
+		this.formParentElement.appendChild(newChild());
 	}
 }
