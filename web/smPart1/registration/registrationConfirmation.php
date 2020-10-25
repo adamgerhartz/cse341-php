577ae40edf1e46667cc844652a217a39779fae14
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-10-24 23:38:50 -0600

    Confirmation page

---
+++ web/smPart1/registration/registrationConfirmation.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Confirmed!</title>
+</head>
+<body>
+
+	<h1>Success! Your have been successfully registered.</h1>
+	<a href='../login/login.html'>Return to login screen</a>
+
+</body>
+</html>
