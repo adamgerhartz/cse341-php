c5cebcedd1c2243aa07f51ddfe4d7403f5a21fd3
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-10-25 01:01:08 -0600

    under construction

--- web/smPart1/home/home.php
+++ web/smPart1/home/home.php
@@ -1,4 +1,25 @@
 <?php
 session_start();
-echo $_SESSION['user_id'];
+
+$db = require('config.php');
+
+$stmt = $db->prepare('SELECT first_name, last_name FROM public.user WHERE user_id=:id');
+$stmt->bindValue(':id', $_SESSION['user_id'], PDO::PARAM_INT);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$firstName = $rows[0]['first_name'];
+$lastName = $rows[0]['last_name'];
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+
+	<h1>Hi <?php echo $firstName . ' ' . $lastName;?>, your profile page is under construction.</h1>
+
+</body>
+</html>
