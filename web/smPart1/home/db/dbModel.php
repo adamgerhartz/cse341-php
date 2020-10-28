f8ba73652057dba0745dd112c0c9ec636a9ba53b
Adam Gerhartz <adamgerhartz@Adams-MacBook-Pro.local>
2020-10-28 04:23:10 -0600

    test

--- web/smPart1/home/db/dbModel.php
+++ web/smPart1/home/db/dbModel.php
@@ -6,15 +6,16 @@ $db = require('../db/config.php');
 class DbModel {
 
     function retrieveFirstName($id) {
+        global $db;
         $stmt = $db->prepare('SELECT first_name FROM public.user WHERE user_id=:id');
-        //$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-        //$result = $stmt->execute();
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $result = $stmt->execute();
 
-        // if ($result) {
-        //     $row = $stmt->fetch();
-        //     $firstName = $row['first_name'];
-        //     echo $firstName;
-        // }
+        if ($result) {
+            $row = $stmt->fetch();
+            $firstName = $row['first_name'];
+            echo $firstName;
+        }
     }
 
  }
