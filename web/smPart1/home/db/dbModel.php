eb45113dc9d85690629bb25b55ee0ab8ad83cc71
Adam Gerhartz <adamgerhartz@Adams-MacBook-Pro.local>
2020-10-28 04:43:52 -0600

    test

--- web/smPart1/home/db/dbModel.php
+++ web/smPart1/home/db/dbModel.php
@@ -7,7 +7,6 @@ class DbModel {
 
     function retrieveFirstName($id) {
         echo 'Adam';
-        echo 'Gerhartz';
         // global $db;
         // $stmt = $db->prepare('SELECT first_name, last_name FROM public.user WHERE user_id=:id');
         // $stmt->bindValue(':id', $id, PDO::PARAM_INT);
