b4f2c0974bf942ca5880926b429ce8df562eddf1
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-01 20:48:59 -0700

    Add error

--- web/smPart1/edit/editController.php
+++ web/smPart1/edit/editController.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$_SESSION['error'] = '';
 require('editModel.php');
 require('editView.php');
 
--- web/smPart1/edit/editView.php
+++ web/smPart1/edit/editView.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$_SESSION['error'] = '';
 
 class EditView {
     function __construct($parentId) {
