9bff1dac542ffdf1b5a8a69147b484f16db73b88
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-01 20:43:43 -0700

    Add logic error

--- web/smPart1/edit/editView.php
+++ web/smPart1/edit/editView.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$_SESSION['error'] = '';
 
 class EditView {
     function __construct($parentId) {
@@ -29,7 +30,7 @@ class EditView {
         echo '<div class="col-sm-1"></div>';
         echo '<div class="col" id="text">';
         echo '<h1><strong>Edit your profile...</strong></h1>';
-        if ($_SESSION['error'] !== '' || (isset($_SESSION['error']))) {
+        if ($_SESSION['error'] !== '') {
             echo '<small id="msg-disable" class="form-text text-muted">* ' . $_SESSION['error'] . '</small>';
         }
         echo '</div>'; // end text container
