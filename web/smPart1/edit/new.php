dfb771d712be156c58c26d0c602cdff0f608ea2d
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-01 19:06:38 -0700

    add

--- web/smPart1/edit/new.php
+++ web/smPart1/edit/new.php
@@ -10,7 +10,10 @@
 <body>
 
 <?php
+    
+    echo $_POST['first_name'] . ' ' . $_POST['last_name'];
     if (!isset($_POST['first_name']) || !isset($_POST['last_name'])) {
+        echo 'Function Called';
         header('Location: edit-profile.php');
     }
 ?>
