6c43793a1a7ec252c8c8c9fd78aebc59f2aaf1d9
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-01 22:27:05 -0700

    Cosmetic bug

--- web/smPart1/home/homeView.php
+++ web/smPart1/home/homeView.php
@@ -30,7 +30,7 @@ class HomeView {
         echo '<img id="profile_picture" src="' . ((isset($data['photo_uri'])) ? $data['photo_uri'] : $default) . '" alt="Profile picture of ' . $data['first_name'] . ' ' . $data['last_name'] . '">';
         echo '</div>'; // end profile_back
         
-        //echo '<div class="col-sm-1"></div>';
+        echo '<div class="col-sm-1"></div>';
         echo '<div class="col float-left" id="text">';
 		echo '<h1><strong>' . $data['first_name'] . ' ' . $data['last_name'] . '</strong></h1>';
         echo '<h6 id="author">' . $data['about_me'] . '</h6>';
