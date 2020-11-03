ab6802e4265f4e995d87cba98f464a9d37cafe85
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2020-11-03 05:13:31 -0700

    Add styling

--- web/smPart1/registration/registrationConfirmation.php
+++ web/smPart1/registration/registrationConfirmation.php
@@ -2,11 +2,15 @@
 <html>
 <head>
 	<title>Confirmed!</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel='stylesheet' href='register.css'>
 </head>
 <body>
 
+<main class="container">
 	<h1>Success! Your have been successfully registered.</h1>
 	<a href='../login/login.html'>Return to login screen</a>
+</main>
 
 </body>
 </html>
